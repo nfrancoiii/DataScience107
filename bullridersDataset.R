4fb7f2099ef965b60d7f600c76fefe9c4fdf97bc
Franco <Nicholas.Franco@owg.ds.corp>
2016-02-19 17:40:03 -0500

    Bull Rider Problem Set Partial

--- bullridersDataset.R
+++ bullridersDataset.R
@@ -15,3 +15,48 @@ cor(bull$Events, bull$BuckOuts)
 #Correlation Matrix
 myvars <- c("YearsPro", "Events", "BuckOuts")
 cor(bull[, myvars])
+
+#Subset for riders that participated in at least one event in 2013
+new_bull <- bull[bull$Events13  > 0 ,]
+
+# Visualize and describe the first variable of interest
+hist(new_bull$Rides13)
+fivenum(new_bull$Rides13)
+mean(new_bull$Rides13)
+sd(new_bull$Rides13)
+
+# Visualize and describe the second variable of interest
+hist(new_bull$Top10_13)
+fivenum(new_bull$Top10_13)
+mean(new_bull$Top10_13)
+sd(new_bull$Top10_13)
+
+# Create a scatterplot
+plot(new_bull$Rides13,new_bull$Top10_13)
+
+# Add line of best fit
+abline(lm(new_bull$Top10_13~new_bull$Rides13))
+
+# Calculate the correlation coefficient
+cor(new_bull$Rides13,new_bull$Top10_13)
+
+# Create a correlation matrix 
+vars <- c("Top10_13", "Rides13")
+cor(new_bull[,vars])
+
+###############################
+#Subset for riders that participated in at least one event in 2012
+new_bull12 <- bull[bull$Events12  > 0 ,]
+
+hist(new_bull12$Earnings12)
+fivenum(new_bull12$Earnings12)
+#Make a correlation matrix
+vars12 <- c("Earnings12", "RidePer12", "CupPoints12")
+cor(new_bull12[,vars12])
+
+#################################
+##Problem Set
+
+new_bull14 <- bull[bull$Rides14,]
+
+plot(new_bull14$)
